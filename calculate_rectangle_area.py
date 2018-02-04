80a3d84dc92a18ebeff14b6a45b33b8b03edd57d
littleredtootle <littleredtootle@gmail.com>
2018-02-04 19:59:34 +1300

    New practice file

---
+++ calculate_rectangle_area.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 21:51:06 2018
+
+@author: Michelle Jump
+"""
+
+##Create a function that calculates and outputs the rectangle area 
+##given its two sides as function arguments.
+
+#Note that there should not be gaps between arguments; this results in the
+#code not running.
+def rectangle_area(length,width):
+    area = length * width
+    return area
+
+print(rectangle_area(2,4))
+    
+
+    
+    
