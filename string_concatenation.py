268454545666fbc5e215c7734ada8f5fa27bcf79
littleredtootle <littleredtootle@gmail.com>
2018-02-04 20:23:05 +1300

    New practice file

---
+++ string_concatenation.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 20:09:47 2018
+
+@author: Michelle Jump
+"""
+def string_concatenation(string1, string2):
+    string = string1 + " " + string2
+    return string
+
+print(string_concatenation("John", "Smith"))
