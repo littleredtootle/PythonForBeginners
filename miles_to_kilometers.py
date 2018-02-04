615e95d476f220fbd43823281639074ca78eb9ee
littleredtootle <littleredtootle@gmail.com>
2018-02-04 20:08:15 +1300

    New practice file

---
+++ miles_to_kilometers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 22:27:31 2018
+
+@author: Michelle Jump
+"""
+
+#Calculate miles to kilometers and print the answer.
+
+def miles_to_kilometers(miles):
+    kilometers = miles * 1.6
+    return kilometers
+
+print(miles_to_kilometers(1))
