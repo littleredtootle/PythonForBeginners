ff807660dca98d3fd3713988d48d9d8fa1d089c2
Ramanpreet Singh <50739802+raman3035@users.noreply.github.com>
2020-10-01 05:55:28 +0530

    Create Marks.py

---
+++ Marks.py
@@ -0,0 +1,8 @@
+number=(int(input("ENTER YOUR MARKS")))
+for number in range(number<=32):
+    print("you are fail")
+for number in range(number>50):
+    print("YOU HAVE GOT D GRADE")
+for number in range(number>70 & number<51):
+    print("YOU HAVE GOT C GRADE")
+    
